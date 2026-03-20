78012648eabcda70b090abe522b21277f1ef372a
Soniya Mongar <08250285.sce@rub.edu.bt>
2026-03-20 16:15:11 +0600

    student activity

---
+++ Demo/studentactivityEx.py
@@ -0,0 +1,14 @@
+print("Hello user")
+name = input("Enter your name:");
+print (name)
+x = float(input("Enter num1"));
+y = float(input("Enter num2"));
+addition_result = x + y 
+print(addition_result)
+subtraction_result = x - y
+print(subtraction_result)
+multiplication_result = x * y
+print(multiplication_result)
+division_result = x / y
+print(division_result)
+print("Thank you for your time")
