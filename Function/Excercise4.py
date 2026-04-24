083da9f0cfa78dfc041950dc541d391b5852b854
Soniya Mongar <08250285.sce@rub.edu.bt>
2026-04-24 16:35:07 +0600

    Excercise 4

---
+++ Function/Excercise4.py
@@ -0,0 +1,44 @@
+# Functions for operations
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        return "Error: Division by zero is indefined."
+    else:
+        return a / b
+
+while True:
+    print(" Simple Calculator")
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+    print("5. Exit")
+
+    choice = input("Enter your choice (1-5): ")
+
+    if choice == "5":
+        print("Exiting calculator. bye bye!")
+        break
+
+    if choice in ("1", "2", "3", "4"):
+        a = int(input("Enter the first number: "))
+        b = int(input("Enter the second number: "))
+
+        if choice == "1":
+            print("Result:", add(a, b))
+        elif choice == "2":
+            print("Result:", subtract(a, b))
+        elif choice == "3":
+            print("Result:", multiply(a, b))
+        elif choice == "4":
+            print("Result:", divide(a, b))
+    else:
+        print("Invalid choice. Please select from 1 to 5.")
